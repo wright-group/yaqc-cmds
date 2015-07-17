a819c0958e73434e7331cf53c274af25e1548339
neffmallon <neffmallon@users.noreply.github.com>
2015-07-16 21:58:26 -0500

    Fixing my weird file-deleting commit
    I don't know what happened with my last commit, but this should fix it.

--- opas/opas.py
+++ opas/opas.py
@@ -1,1 +1,33 @@
+### import ####################################################################
+
+
+import time
+import collections
+
+from PyQt4 import QtCore
+
+import project.project_globals as g
+main_dir = g.main_dir.read()
+app = g.app.read()
+import project.widgets as pw
+import project.ini_handler as ini
+spectrometers_ini = ini.spectrometers
+import project.classes as pc
+
+import copy
+
+
+### address ###################################################################
+
+
+class OPA(pc.Address):
+
+    def dummy(self):
+        print 'hello world im a dummy method'
+
+hardwares = []
+
+### gui #######################################################################
+
+
 gui = pw.HardwareFrontPanel(hardwares, name='OPAs')
