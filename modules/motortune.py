80d183857d7a3bce98a67641f7303858fe18aa13
neffmallon <mallonna@gmail.com>
2015-09-30 10:31:08 -0500

    Revert "Motortune update"
    This reverts commit 06f2c4d6e249ef02a6c4642143fa8d169bc26be5.

--- modules/motortune.py
+++ modules/motortune.py
@@ -36,7 +36,7 @@ class fraction_complete:
         self.value = 0
     def read(self):
         return self.value
-    def write(self, value):
+    def write(self, value):  
         self.value = value
 fraction_complete = fraction_complete()
 
@@ -45,7 +45,7 @@ class go:
         self.value = False
     def read(self):
         return self.value
-    def write(self, value):
+    def write(self, value):  
         self.value = value
 go = go()
 
@@ -102,14 +102,14 @@ paused = paused()
 class scan(QtCore.QObject):
     update_ui = QtCore.pyqtSignal()
     done = QtCore.pyqtSignal()
-
+    
     @QtCore.pyqtSlot(list)
     def run(self, inputs):
-
+        
         # unpack inputs -------------------------------------------------------
-
+        
         scan_dictionary = inputs[0]
-
+        
         daq_widget = inputs[1]
 
         # startup -------------------------------------------------------------
@@ -122,31 +122,26 @@ class scan(QtCore.QObject):
         # scan ----------------------------------------------------------------
 
         spec_destinations = np.linspace(1100, 1620, 200)
-        grating_destinations = np.linspace(32, 40, 40)
-        bbo_points = 15
-        bbo_half_width = .2
-        bbo_equ = lambda gra: -0.7907*gra+66.727 #center of bbo given grating for OPA1 (08/14/2015)
-
-        npts = len(spec_destinations)*bbo_points*len(grating_destinations)
+        grating_destinations = np.linspace(34, 40, 40)
+        bbo_destinations = np.linspace(35, 40, 80)
+        
+        npts = len(spec_destinations)*len(bbo_destinations)*len(grating_destinations)
 
         # initialize scan in daq
         daq.control.initialize_scan(daq_widget, fit=False)
-
+        
         # do loop
         break_scan = False
         idx = 0
         for k in range(len(grating_destinations)):
-            # Use the BBO perameters above to get the center point, width, etc.
-            center = bbo_equ(grating_destinations[k])
-            bbo_destinations = np.linspace(center-bbo_half_width,center+bbo_half_width,bbo_points)
-            for j in range(bbo_points):
-                inputs = [grating_destinations[k], bbo_destinations[j], -1.] #negative numbers don't move moter
-                OPA1.q.push('set_motors', inputs)
+            for j in range(len(bbo_destinations)):    
+                inputs = [grating_destinations[k], bbo_destinations[j], 16.]
+                OPA2.q.push('set_motors', inputs)    
                 # slice index
                 daq.control.index_slice(col='MicroHR')
-                daq_current.gui.set_xlim(spec_destinations.min(), spec_destinations.max())
+                daq_current.gui.set_xlim(spec_destinations.min(), spec_destinations.max())    
                 for i in range(len(spec_destinations)):
-                    # set mono
+                    # set mono        
                     MicroHR.set_position(spec_destinations[i], 'nm')
                     # wait for all hardware
                     g.hardware_waits.wait()
@@ -167,36 +162,36 @@ class scan(QtCore.QObject):
                 #daq.control.fit('MicroHR', 'vai0 Mean')
             if break_scan:
                 break
-
+        
         #end-------------------------------------------------------------------
 
         print 'end'
-        fraction_complete.write(1.)
+        fraction_complete.write(1.)    
         going.write(False)
         g.module_control.write(False)
         g.logger.log('info', 'Scan done', 'some info describing this scan')
         self.update_ui.emit()
         self.done.emit()
-
+        
     def check_continue(self):
         '''
-        you should put this method into your scan loop wherever you want to check
+        you should put this method into your scan loop wherever you want to check 
         for pause or stop commands from the main program
-
+        
         at the very least this method MUST go into your innermost loop
-
+        
         for loops, use it as follows: if not self.check_continue(): break
         '''
-        while pause.read():
+        while pause.read(): 
             paused.write(True)
             pause.wait_for_update()
         paused.write(False)
         return go.read()
-
-#move scan to own thread
+        
+#move scan to own thread      
 scan_obj = scan()
 scan_obj.moveToThread(scan_thread)
-
+ 
 ### gui #######################################################################
 
 class gui(QtCore.QObject):
@@ -208,18 +203,18 @@ class gui(QtCore.QObject):
         self.create_advanced_frame()
         self.show_frame() #check once at startup
         g.shutdown.read().connect(self.stop)
-
+        
     def create_frame(self):
         layout = QtGui.QVBoxLayout()
         layout.setMargin(5)
-
+        
         allowed = [hardware.name for hardware in opas.hardwares]
         self.opa_combo = pc.Combo(allowed)
-
+        
         input_table = pw.InputTable()
         input_table.add('OPA', self.opa_combo)
         layout.addWidget(input_table)
-
+        
         motor_names = ['grating', 'bbo', 'mixer']
         input_table = pw.InputTable()
         for i in range(len(motor_names)):
@@ -231,25 +226,25 @@ class gui(QtCore.QObject):
             input_table.add('Center', center)
             input_table.add('Number', npts)
         layout.addWidget(input_table)
-
+        
         # daq widget
         self.daq_widget = daq.Widget()
         layout.addWidget(self.daq_widget)
-
+        
         # go button
         self.go_button = pw.module_go_button()
         self.go_button.give_launch_scan_method(self.launch_scan)
-        self.go_button.give_stop_scan_method(self.stop)
+        self.go_button.give_stop_scan_method(self.stop)  
         self.go_button.give_scan_complete_signal(scan_obj.done)
         self.go_button.give_pause_objects(pause, paused)
-
+        
         layout.addWidget(self.go_button)
-
+        
         layout.addStretch(1)
-
+        
         self.frame = QtGui.QWidget()
         self.frame.setLayout(layout)
-
+        
         g.module_widget.add_child(self.frame)
         g.module_combobox.add_module(module_name, self.show_frame)
 
@@ -257,9 +252,9 @@ class gui(QtCore.QObject):
         layout = QtGui.QVBoxLayout()
         layout.setMargin(5)
 
-        self.advanced_frame = QtGui.QWidget()
+        self.advanced_frame = QtGui.QWidget()   
         self.advanced_frame.setLayout(layout)
-
+        
         g.module_advanced_widget.add_child(self.advanced_frame)
 
     def show_frame(self):
@@ -269,21 +264,21 @@ class gui(QtCore.QObject):
             self.frame.show()
             self.advanced_frame.show()
 
-    def launch_scan(self):
+    def launch_scan(self):        
         go.write(True)
         print 'running'
         scan_dictionary = {}
         inputs = [scan_dictionary, self.daq_widget]
-        QtCore.QMetaObject.invokeMethod(scan_obj, 'run', QtCore.Qt.QueuedConnection, QtCore.Q_ARG(list, inputs))
-        g.progress_bar.begin_new_scan_timer()
-
+        QtCore.QMetaObject.invokeMethod(scan_obj, 'run', QtCore.Qt.QueuedConnection, QtCore.Q_ARG(list, inputs))    
+        g.progress_bar.begin_new_scan_timer()        
+        
     def update(self):
         g.progress_bar.set_fraction(fraction_complete.read())
-
+              
     def stop(self):
         print 'stopping'
         go.write(False)
         while going.read(): going.wait_for_update()
         print 'stopped'
-
+        
 gui = gui()
