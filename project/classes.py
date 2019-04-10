7e158ae95f4495c1c04a637762bfbed145f2d5fc
Kyle Sunden <sunden@wisc.edu>
2019-04-10 11:41:58 -0500

    Remove future imports as we no longer support py2.7

--- project/classes.py
+++ project/classes.py
@@ -1,8 +1,6 @@
 ### import ####################################################################
 
 
-from __future__ import division, print_function, unicode_literals
-
 import os
 import time
 
--- somatic/acquisition.py
+++ somatic/acquisition.py
@@ -6,8 +6,6 @@ Acquisition infrastructure shared by all modules.
 ### import ####################################################################
 
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import re
 import os
 import sys
