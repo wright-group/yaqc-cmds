94b70863da68760134d87e900d6b07799210ac5d
Kyle Sunden <sunden@wisc.edu>
2019-04-10 16:59:18 -0500

    Zero Tune enhancements (#216)
    * Fix graphical layout glitches
    
    * Add option for number of points

--- somatic/modules/zero_tune.py
+++ somatic/modules/zero_tune.py
@@ -66,13 +66,15 @@ class Worker(acquisition.Worker):
         axes = []
         # OPA
         opa_name = self.aqn.read('opa', 'opa')
+        npts = self.aqn.read('opa', 'npts')
         opa_names = [opa.name for opa in opas.hardwares]
         opa_index = opa_names.index(opa_name)
         opa_hardware = opas.hardwares[opa_index]
         opa_friendly_name = opa_hardware.name
         curve = opa_hardware.curve.copy()
         curve.convert('wn')
-        axis = acquisition.Axis(curve.setpoints[:], 'wn', opa_friendly_name, opa_friendly_name)
+        pts = np.linspace(curve.setpoints[:].min(), curve.setpoints[:].max(), npts)
+        axis = acquisition.Axis(pts, 'wn', opa_friendly_name, opa_friendly_name)
         axes.append(axis)
         # delay
         axis_name = 'delay'
@@ -116,15 +118,19 @@ class GUI(acquisition.GUI):
         self.opa_combo = pc.Combo(allowed)
         input_table.add('OPA', None)
         input_table.add('OPA', self.opa_combo)
+        self.npts_opa = pc.Number(decimals=0, initial_value=21)
+        input_table.add("npts", self.npts_opa)
         # delay
         self.delay = ScanAxisGUI('delay', "")
         self.delay.start.write(-3)
         self.delay.stop.write(3)
         self.delay.number.write(21)
         input_table.add('Delay', None)
+        self.layout.addWidget(input_table)
         self.layout.addWidget(self.delay.widget)
         # constants
         self.constants = []
+        input_table = pw.InputTable()
         input_table.add('Constants', None)
         self.layout.addWidget(input_table)
         self.constants_container_widget = QtGui.QWidget()
@@ -135,6 +141,7 @@ class GUI(acquisition.GUI):
         add_constant_button.clicked.connect(self.add_constant)
         remove_constant_button.clicked.connect(self.remove_constant)
         # processing
+        input_table = pw.InputTable()
         input_table.add('Processing', None)
         self.channel_combo = pc.Combo(allowed_values=devices.control.channel_names, ini=ini, section='main', option='channel name')
         input_table.add('Channel', self.channel_combo)
@@ -177,6 +184,7 @@ class GUI(acquisition.GUI):
         self.constants = []
         aqn = wt.kit.INI(aqn_path)
         self.opa_combo.write(aqn.read('opa', 'opa'))
+        self.npts_opa.write(aqn.read('opa', 'npts'))
         self.mono_width.write(aqn.read('spectrometer', 'width'))
         self.mono_npts.write(aqn.read('spectrometer', 'number'))
         self.channel_combo.write(aqn.read('processing', 'channel'))
@@ -206,6 +214,7 @@ class GUI(acquisition.GUI):
         aqn = wt.kit.INI(aqn_path)
         aqn.add_section('opa')
         aqn.write('opa', 'opa', self.opa_combo.read())
+        aqn.write('opa', 'npts', self.npts_opa.read())
         aqn.add_section('delay')
         aqn.write('delay', 'start', self.delay.start.read())
         aqn.write('delay', 'stop', self.delay.stop.read())
