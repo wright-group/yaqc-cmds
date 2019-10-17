96bb7df84a10db921c0ae03cc77dda29421f4475
Kyle Sunden <sunden@wisc.edu>
2019-10-17 18:06:08 -0500

    Add array detector support to tune test

--- somatic/modules/tune_test.py
+++ somatic/modules/tune_test.py
@@ -20,6 +20,7 @@ ini = ini_handler.Ini(os.path.join(main_dir, 'somatic', 'modules', 'tune_test.in
 app = g.app.read()
 
 import hardware.opas.opas as opas
+import hardware.spectrometers.spectrometers as spectrometers
 import devices.devices as devices
 
  
@@ -46,16 +47,21 @@ class Worker(acquisition.Worker):
         if curve.kind == 'poynting':
             curve = curve.subcurve
         channel_name = self.aqn.read('processing', 'channel')
-        try:
-            order = int(self.aqn.read('spectrometer', 'order'))
-        except KeyError:
-            order = 1
+        order = int(self.aqn.read('spectrometer', 'order'))
         transform = list(data.axis_names)[:2]
-        if order > 0:
-            transform[1] = f"{transform[-1]}_points/{order}"
-        else:
-            transform[1] = f"{transform[-1]}_points*{abs(order)}"
+        spectral_axis = self.aqn.read("processing", "spectral_axis")
+        if self.aqn.read("spectrometer", "action") == "Scanned":
+            spectral_axis = self.aqn.read("spectrometer", "spectrometer")
+        transform = [opa_name, f"{spectral_axis}-{opa_name}"]
+        if order != 1:
+            if order > 0:
+                transform[1] = f"({transform[-1]})/{order}"
+            else:
+                transform[1] = f"({transform[-1]})*{abs(order)}"
+
+
         data.transform(*transform)
+        data.print_tree()
         attune.workup.tune_test(
             data,
             channel_name,
@@ -65,7 +71,7 @@ class Worker(acquisition.Worker):
             gtol=self.aqn.read("processing", "gtol"),
             ltol=self.aqn.read("processing", "ltol"),
         )
-        if not self.stopped.read() and self.aqn.read("processing", "apply"):
+        if not self.stopped.read() and self.aqn.read("processing", "apply_curve"):
             p = wt.kit.glob_handler('.curve', folder = str(scan_folder))[0]
             self.opa_hardware.driver.curve_paths[self.curve_id].write(p)
         # upload
@@ -83,26 +89,34 @@ class Worker(acquisition.Worker):
         if curve.kind == 'poynting':
             curve = curve.subcurve
         curve.convert('wn')
-        axis = acquisition.Axis(curve.setpoints[:], 'wn', opa_friendly_name, opa_friendly_name)
-        axes.append(axis)
-        # mono
-        name = 'wm'
-        identity = 'Dwm'
-        try:
-            order = self.aqn.read('spectrometer', 'order')
-        except KeyError:
-            order = 1
-        if order == 0:
-            raise ValueError("Spectrometer order cannot be 0")
-        elif order > 0:
-            kwargs = {'centers': curve.setpoints[:] * self.aqn.read('spectrometer', 'order')}
+        spectrometer = self.aqn.read("spectrometer", "spectrometer")
+
+        if self.aqn.read("spectrometer", "action") == "Scanned":
+            axis = acquisition.Axis(curve.setpoints[:], 'wn', opa_friendly_name, opa_friendly_name)
+            axes.append(axis)
+            # mono
+            identity = f'D{spectrometer}'
+            try:
+                order = self.aqn.read('spectrometer', 'order')
+            except KeyError:
+                order = 1
+            if order == 0:
+                raise ValueError("Spectrometer order cannot be 0")
+            elif order > 0:
+                kwargs = {'centers': curve.setpoints[:] * self.aqn.read('spectrometer', 'order')}
+            else:
+                kwargs = {'centers': curve.setpoints[:] / abs(self.aqn.read('spectrometer', 'order'))}
+            width = self.aqn.read('spectrometer', 'width')/2.
+            npts = self.aqn.read('spectrometer', 'number')
+            points = np.linspace(-width, width, npts)
+            axis = acquisition.Axis(points, 'wn', spectrometer, identity, **kwargs)
+            axes.append(axis)
+        elif self.aqn.read("spectrometer", "action").startswith("Tracking"):
+            axis = acquisition.Axis(curve.setpoints[:], 'wn', f"{opa_friendly_name}={spectrometer}", f"{opa_friendly_name}={spectrometer}")
+            axes.append(axis)
         else:
-            kwargs = {'centers': curve.setpoints[:] / abs(self.aqn.read('spectrometer', 'order'))}
-        width = self.aqn.read('spectrometer', 'width')/2.
-        npts = self.aqn.read('spectrometer', 'number')
-        points = np.linspace(-width, width, npts)
-        axis = acquisition.Axis(points, 'wn', name, identity, **kwargs)
-        axes.append(axis)
+            axis = acquisition.Axis(curve.setpoints[:], 'wn', opa_friendly_name, opa_friendly_name)
+            axes.append(axis)
         # do scan
         self.scan(axes)
         # finish
@@ -124,6 +138,11 @@ class GUI(acquisition.GUI):
         input_table.add('OPA', self.opa_combo)
         # mono
         input_table.add('Spectrometer', None)
+        self.spec_action = pc.Combo(["Scanned", "None (array detected)", "Tracking (array detected)"], initial_value = "Scanned")
+        self.spec_action.updated.connect(self.on_spec_action_updated)
+        self.spectrometer = pc.Combo(allowed_values=[h.name for h in spectrometers.hardwares])
+        input_table.add("Action", self.spec_action)
+        input_table.add("Spectrometer", self.spectrometer)
         self.mono_width = pc.Number(ini=ini, units='wn',
                                     section='main', option='mono width (wn)',
                                     import_from_ini=True, save_to_ini_at_shutdown=True)
@@ -135,6 +154,16 @@ class GUI(acquisition.GUI):
         input_table.add('Order', self.mono_order)
         # processing
         input_table.add('Processing', None)
+        allowed = []
+        for dev in devices.control.devices:
+            if dev.has_map:
+                allowed.extend(dev.map_axes.keys())
+        if not allowed:
+            allowed = [""]
+        self.spectral_axis = pc.Combo(allowed_values=allowed)
+        self.spectral_axis.set_disabled(True)
+        input_table.add("Spectral Axis", self.spectral_axis)
+
         self.channel_combo = pc.Combo(allowed_values=devices.control.channel_names, ini=ini, section='main', option='channel name')
         self.level = pc.Bool(initial_value=False)
         self.gtol = pc.Number(initial_value=1e-3, decimals=5)
@@ -154,10 +183,10 @@ class GUI(acquisition.GUI):
         self.opa_combo.write(aqn.read('opa', 'opa'))
         self.mono_width.write(aqn.read('spectrometer', 'width'))
         self.mono_npts.write(aqn.read('spectrometer', 'number'))
-        if aqn.has_option('spectrometer', 'order'):
-            self.mono_order.write(aqn.read('spectrometer', 'order'))
-        else:
-            self.mono_order.write(1)
+        self.mono_order.write(aqn.read('spectrometer', 'order'))
+        self.spectrometer.write(aqn.read('spectrometer', 'spectrometer'))
+        self.spec_action.write(aqn.read('spectrometer', 'action'))
+        self.spectral_axis.write(aqn.read('processing', 'spectral_axis'))
         self.channel_combo.write(aqn.read('processing', 'channel'))
         self.level.write(aqn.read("processing", "level"))
         self.ltol.write(aqn.read("processing", "ltol"))
@@ -168,6 +197,14 @@ class GUI(acquisition.GUI):
         
     def on_device_settings_updated(self):
         self.channel_combo.set_allowed_values(devices.control.channel_names)
+
+    def on_spec_action_updated(self):
+        scanned = self.spec_action.read() == "Scanned"
+        self.spectrometer.set_disabled(self.spec_action.read().startswith("None"))
+        self.mono_width.set_disabled(not scanned)
+        self.mono_npts.set_disabled(not scanned)
+        self.mono_order.set_disabled(not scanned)
+        self.spectral_axis.set_disabled(scanned)
         
     def save(self, aqn_path):
         aqn = wt.kit.INI(aqn_path)
@@ -178,12 +215,15 @@ class GUI(acquisition.GUI):
         aqn.write('spectrometer', 'width', self.mono_width.read())
         aqn.write('spectrometer', 'number', self.mono_npts.read())
         aqn.write('spectrometer', 'order', self.mono_order.read())
+        aqn.write('spectrometer', 'spectrometer', self.spectrometer.read())
+        aqn.write('spectrometer', 'action', self.spec_action.read())
         aqn.add_section('processing')
         aqn.write('processing', 'channel', self.channel_combo.read())
         aqn.write('processing', 'level', self.level.read())
         aqn.write('processing', 'gtol', self.gtol.read())
         aqn.write('processing', 'ltol', self.ltol.read())
         aqn.write('processing', 'apply_curve', self.apply_curve.read())
+        aqn.write('processing', 'spectral_axis', self.spectral_axis.read())
         # allow devices to write settings
         self.device_widget.save(aqn_path)
         
