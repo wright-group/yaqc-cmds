5ac0fb5877acd771e0cc5c6047ea829df6b983b6
Kyle Sunden (fs computer) <git@ksunden.space>
2019-12-16 16:06:39 -0600

    Use underscores in motor names for TOPAS-C

--- hardware/opas/TOPAS/TOPAS-C.py
+++ hardware/opas/TOPAS/TOPAS-C.py
@@ -11,9 +11,9 @@ from hardware.opas.TOPAS.TOPAS import GUI as BaseGUI
 class Driver(BaseDriver):
     
     def __init__(self, *args, **kwargs):
-        self.motor_names = ['Crystal 1', 'Delay 1', 'Crystal 2', 'Delay 2',
-                            'Mixer 1', 'Mixer 2','Mixer 3']
-        self.curve_indices = {'Base': 1, 'Mixer 1': 2, 'Mixer 2': 3, 'Mixer 3': 4}
+        self.motor_names = ['Crystal_1', 'Delay_1', 'Crystal_2', 'Delay_2',
+                            'Mixer_1', 'Mixer_2','Mixer_3']
+        self.curve_indices = {'Base': 1, 'Mixer_1': 2, 'Mixer_2': 3, 'Mixer_3': 4}
         self.kind = 'TOPAS-C'
         BaseDriver.__init__(self, *args, **kwargs)
 
