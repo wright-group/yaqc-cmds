af17faafb444f54b8ed6987b486d479973bae9c4
Kyle Sunden <git@ksunden.space>
2020-08-15 01:37:40 -0500

    Add edit config cli subcommand

--- pycmds/__main__.py
+++ pycmds/__main__.py
@@ -7,6 +7,7 @@ import sys
 
 import appdirs  # type: ignore
 import click
+import toml
 
 from .__version__ import __version__
 from ._pycmds import app, MainWindow, style
@@ -14,8 +15,10 @@ from ._pycmds import app, MainWindow, style
 
 @click.group(invoke_without_command=True)
 @click.version_option(__version__)
-def main():
-    launch()
+@click.pass_context
+def main(ctx):
+    if ctx.invoked_subcommand is None:
+        launch()
 
 
 @main.command()
@@ -26,6 +29,36 @@ def launch():
     window.show()
     window.showMaximized()
     app.exec_()
+
+
+@main.command(name="edit-config")
+def edit_config():
+    config_filepath = pathlib.Path(appdirs.user_config_dir("pycmds", "pycmds")) / "config.toml"
+    config_filepath.parent.mkdir(parents=True, exist_ok=True)
+    if not config_filepath.exists():
+        if click.confirm(
+            "No config file found, would you like to use the default (virtual) configuration?",
+            default=True,
+        ):
+            config_template = pathlib.Path(__file__).parent / "config-template.toml"
+            config_filepath.write_text(config_template.read_text())
+
+    while True:
+        if sys.platform.startswith("win32"):
+            config_filepath = str(config_filepath)
+            subprocess.run([os.environ.get("EDITOR", "notepad.exe"), config_filepath])
+        else:
+            subprocess.run([os.environ.get("EDITOR", "vi"), config_filepath])
+        try:
+            toml.load(config_filepath)
+            break
+        except Exception as e:
+            print(e, file=sys.stderr)
+
+            if not click.confirm(
+                "Error parsing config toml. Would you like to re-edit?", default=True,
+            ):
+                break
 
 
 if __name__ == "__main__":
