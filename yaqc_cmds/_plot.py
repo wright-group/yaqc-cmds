Fix live plot units

yaqc_cmds/_plot.py
@@ -109,7 +109,7 @@ class GUI(QtCore.QObject):
         with somatic._wt5.data_container as data:
             axis = data[self.axis.read()]
             units = axis.attrs.get("units")
-            units = list(wt.units.get_valid_conversions(units))
+            units = [units] + list(wt.units.get_valid_conversions(units))
             self.axis_units.set_allowed_values(units)
 
     def on_data_file_written(self):
@@ -124,7 +124,7 @@ class GUI(QtCore.QObject):
             axis = data[self.axis.read()]
             limits = list(
                 wt.units.convert(
-                    [np.min(axis.full), np.max(axis.full)], axis.attrs.get("units"), x_units
+                    [np.nanmin(axis.full), np.nanmax(axis.full)], axis.attrs.get("units"), x_units
                 )
             )
             channel = data[self.channel.read()]
@@ -147,7 +147,8 @@ class GUI(QtCore.QObject):
             try:
                 self.plot_widget.set_xlim(min(limits), max(limits))
                 self.plot_widget.set_ylim(np.min(channel), np.max(channel))
-            except Exception:
+            except Exception as e:
+                print(e)
                 pass
 
     def on_sensors_changed(self):